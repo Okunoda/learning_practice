9413d3cf9e22f2debcdc6fcf8bba5313e42ca59d
okunoda <okunoda@outlook.com>
2024-07-31 20:10:10 +0800

    feat：统一返回值

--- springcloud/cloud-provider-payment8001/src/main/java/org/erywim/controller/PayController.java
+++ springcloud/cloud-provider-payment8001/src/main/java/org/erywim/controller/PayController.java
@@ -6,6 +6,7 @@ import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.erywim.entities.Pay;
 import org.erywim.entities.PayDTO;
+import org.erywim.resp.ResultData;
 import org.erywim.service.PayService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
@@ -25,40 +26,39 @@ public class PayController {
 
     @PostMapping("add")
     @Operation(summary = "新增",description = "新增支付流水方法,json串做参数")
-    public String addPay(@RequestBody Pay pay) {
+    public ResultData<String> addPay(@RequestBody Pay pay) {
         log.info("add pay:{}", pay);
         int result = payService.add(pay);
-        return result > 0? "成功插入记录" : "数据插入失败";
+        return ResultData.success(result > 0? "成功插入记录" : "数据插入失败");
     }
 
     @DeleteMapping("del/{id}")
     @Operation(summary = "删除",description = "删除支付流水方法")
-    public Integer delPay(@PathVariable("id") Integer id) {
+    public ResultData<Integer> delPay(@PathVariable("id") Integer id) {
         log.info("del pay:{}", id);
         int delete = payService.delete(id);
-        return delete;
+        return ResultData.success(delete);
     }
 
     @PutMapping("update")
     @Operation(summary = "修改",description = "修改支付流水方法")
-    public String updatePay(@RequestBody PayDTO param) {
+    public ResultData<String> updatePay(@RequestBody PayDTO param) {
         log.info("update param:{}", param);
         Pay pay = new Pay();
         BeanUtils.copyProperties(param,pay);
         int result = payService.update(pay);
-        return result > 0? "成功修改记录" : "数据修改失败";
+        return ResultData.success(result > 0? "成功修改记录" : "数据修改失败");
     }
 
     @GetMapping("get/{id}")
     @Operation(summary = "按照ID查流水",description = "查询支付流水方法")
-    public Pay getPay(@PathVariable("id") Integer id) {
-        return payService.getById(id);
+    public ResultData<Pay> getPay(@PathVariable("id") Integer id) {
+        return ResultData.success(payService.getById(id));
     }
 
     @GetMapping("getAll")
     @Operation(summary = "查所有流水",description = "查询支付流水方法")
-    public List<Pay> getAllPay() {
-        return payService.getAll();
+    public ResultData<List<Pay>> getAllPay() {
+        return ResultData.success(payService.getAll());
     }
-
 }
